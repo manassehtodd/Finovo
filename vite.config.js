12e7f321b2484f9fc14aff209aa39172ce8a022c
manssehtodd <manassehtodd1@gmail.com>
2026-02-20 19:17:58 +0100

    Move base path to top level in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import flowbiteReact from "flowbite-react/plugin/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/Finovo/",   // ✅ MOVE IT HERE (top level)
+
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
-      },
-      base: "/Finovo/"
+      }
     }),
     tailwindcss(),
     flowbiteReact()
