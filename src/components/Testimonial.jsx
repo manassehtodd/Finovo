9a5643058128ff5cb82a1eba9e40bdd140c9a454
manssehtodd <manassehtodd1@gmail.com>
2026-02-26 20:05:12 +0100

    Refactor code structure for improved readability and maintainability

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,61 +1,97 @@
-import Slider from "./Slider.tsx";
-import SliderContent from "./SliderContent.jsx";
-import content from "./Contentdetails.jsx";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useState } from "react";
+import { EffectCoverflow, Navigation } from "swiper/modules";
 
-function renderSliderContent(item) {
-  return (
-    <SliderContent
-      key={item.id}
-      img={item.img}
-      text={item.text}
-      name={item.name}
-    />
-  );
-}
+import reviewImg from "./../assets/images/reviewPic.jpg";
+import reviewImg2 from "./../assets/images/reviewPic2.jpg";
+import reviewImg3 from "./../assets/images/reviewPic3.jpg";
+import reviewImg4 from "./../assets/images/reviewPic4.jpg";
+import reviewImg5 from "./../assets/images/reviewPic5.jpg";
+import reviewImg6 from "./../assets/images/reviewPic6.jpg";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/navigation";
 
 function Testimonial() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const testimonials = [
+    { img: reviewImg, 
+      text: "This app has transformed the way I manage my daily tasks and stay organized.", 
+      name: "John Doe", 
+      rating: "⭐⭐⭐⭐⭐" },
+    { img: reviewImg2, 
+      text: "I love how easy it is to use and customize my tasks.", 
+      name: "Jane Smith", 
+      rating: "⭐⭐⭐⭐" },
+    { img: reviewImg3, 
+      text: "The interface is intuitive and the features are well thought out.", 
+      name: "Michael" , 
+      rating: "⭐⭐⭐⭐⭐"},
+    { img: reviewImg4, 
+      text: "I've been using this app for months and it's been a game-changer.", 
+      name: "Sarah" , 
+      rating: "⭐⭐⭐⭐"},
+    { img: reviewImg5, 
+      text: "This app has helped me stay productive and focused.", 
+      name: "Daniel" , 
+      rating: "⭐⭐⭐⭐⭐"},
+    { img: reviewImg6, 
+      text: "The app is incredibly user-friendly and efficient.", 
+      name: "Emma" , 
+      rating: "⭐⭐⭐⭐⭐"},
+  ];
+
   return (
-    <section
-      id="testimonial"
-      className="bg-gray-100 px-4 sm:px-6 md:px-12 lg:px-20 py-16 md:py-24 overflow-hidden"
-    >
-      {/* HEADER */}
-      <div className="flex justify-center text-center">
-        <div className="max-w-2xl">
-          <p className="text-sm tracking-widest mb-3">TESTIMONIAL</p>
+    <section className="bg-gray-100 py-20 px-6">
+      <div className="mt-12">
 
-          <h1 className="
-            text-2xl 
-            sm:text-3xl 
-            md:text-4xl 
-            lg:text-5xl 
-            font-bold 
-            leading-tight
-          ">
-            USER REVIEWS AND EXPERIENCES
-          </h1>
+        <Swiper
+          onSlideChange={(swiper) =>
+            setActiveIndex(swiper.realIndex)
+          }
+          effect="coverflow"
+          centeredSlides={true}
+          slidesPerView="auto"
+          grabCursor={true}
+          loop={false}
+          modules={[EffectCoverflow, Navigation]}
+          navigation={true}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 150,
+            modifier: 2,
+            slideShadows: false,
+          }}
+        >
+          {testimonials.map((item, index) => (
+            <SwiperSlide key={index} className="w-40!">
+              <img
+                src={item.img}
+                className="h-40 w-40 rounded-xl border-4 border-white object-cover"
+                alt={item.name}
+              />
+            </SwiperSlide>
+          ))}
+          <div className="slider-controler">
+            <div className="swiper-button-prev slider-arrow h-12 w-12"></div>
+          </div>
+        </Swiper>
 
-          <button className="
-            mt-6 
-            px-6 
-            py-3 
-            bg-black 
-            text-white 
-            rounded-full 
-            hover:bg-gray-800 
-            transition
-            cursor-pointer
-          ">
-            Explore All
-          </button>
+        <div className="mt-8 text-center max-w-2xl mx-auto">
+          <span>
+            {testimonials[activeIndex].rating}
+          </span>
+          <p className="text-gray-600 italic">
+            "{testimonials[activeIndex].text}"
+          </p>
+          <p className="mt-2 font-semibold">
+            {testimonials[activeIndex].name}
+          </p>
         </div>
-      </div>
 
-      {/* SLIDER */}
-      <div className="mt-12 md:mt-16">
-        <Slider>
-          {content.map(renderSliderContent)}
-        </Slider>
       </div>
     </section>
   );
